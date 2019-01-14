9219774695ffd01ca6b8b861aa0ae4f800c407a6
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2019-01-14 23:24:38 +0000

    refactor app.js

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 const express = require('express')
 const axios = require('axios')
-
 const Table = require('cli-table')
 
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 
-const getTubeStatus = async () => {
+// API CALL STUFF:
+const getLineStatus = async (lineType) => {
     try {
-        return await axios.get('https://api.tfl.gov.uk/line/mode/tube/status')
+        return await axios.get(`https://api.tfl.gov.uk/line/mode/${lineType}/status`)
     } catch (error) {
         console.log(error)
         return 'SOMETHING BROKE 🤯 SORRY!'
     }
 }
 
-const getBusStatus = async () => {
-    try {
-        return await axios.get('https://api.tfl.gov.uk/line/mode/bus/status')
-    } catch (error) {
-        console.log(error)
-        return 'SOMETHING BROKE 🤯 SORRY!'
-    }
-}
-
+// BUILDERS:
 const buildStatusMessage = (statusData) => {
     const table = new Table({ head: ['Line', 'Status']})
 
@@ -63,12 +55,13 @@ const buildGhettoHTMLVersion = (statusData) => {
     return `<h1>TUBE STATUS:</h1><ul>${list.join('')}</ul><p>NOTE: This is much better if you use "curl tubestat.us" from a terminal</p>`
 }
 
+//ROUTES:
 app.get('/home', (req, res) => res.send('Nothing to see here 👀\n'))
 
 app.get('/health', (req, res) => res.send('APP IS WORKING!\n'))
 
 app.get('/', async (req, res) => {
-    let tubeDataResponse = await getTubeStatus()
+    let tubeDataResponse = await getLineStatus('tube')
     console.log(req.headers["user-agent"])
     console.log('GET TUBE DATA, STATUS: ', tubeDataResponse.status)
 
@@ -83,7 +76,7 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/bus', async (req, res) => {
-    let busDataResponse = await getBusStatus()
+    let busDataResponse = await getLineStatus('bus')
     console.log(req.headers["user-agent"])
     console.log('GET TUBE DATA, STATUS: ', busDataResponse.status)
 
@@ -95,14 +88,6 @@ app.get('/bus', async (req, res) => {
     } else {
         res.send(htmlResponse)
     }
-})
-
-app.get('/html', async (req, res) => {
-    let tubeDataResponse = await getTubeStatus()
-    console.log('GET TUBE DATA, STATUS: ', tubeDataResponse.status)
-
-    const statusMessage = buildGhettoHTMLVersion(tubeDataResponse.data)
-    res.send(statusMessage)
 })
 
 app.listen(port, () => console.log(`tubestat.us is up on port ${port}!`))
